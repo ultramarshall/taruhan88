d11f9073d73f6968a675686526c3e9c21617f6d3
ultramarshall <ugd24jam@gmail.com>
2017-04-24 02:08:46 +0700

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         $('#navigation:checkbox').change(function () {
             // this will contain a reference to the checkbox   
             if (this.checked) {
-                $('nav').css('height', '250px');
+                $('nav').css('height', '259px');
             } else {
                 $('nav').css('height', '0px');
             }
@@ -11,15 +11,20 @@ $(document).ready(function () {
 
     });
     $(window).resize(function () {
-        if ($(this).width() > 1024) {
-            $('nav').css('height', '30px');
-            $('nav').css('top', '0px');
-            $('nav').css('position', 'relative');
+        if ($(this).width() >= 1025) {
+            $('nav').css({
+                'height': '30px',
+                'top': '0px',
+                'position': 'relative'
+            });
         } 
-        if( $(this).width() < 1024) {
-            $('nav').css('height', '0px');
-            $('nav').css('top', '60px');
-            $('nav').css('position', 'absolute');
+        if( $(this).width() <= 1024) {
+            $('nav').css({
+                'height': '0px',
+                'top': '60px',
+                'width': '100%',
+                'position': 'absolute'
+            });
         }
     });
     $(window).scroll(function () {
@@ -31,14 +36,22 @@ $(document).ready(function () {
                 'width': '1024px',
                 'z-index': 2
             });
-            
+            $('aside').css({
+                'position': 'fixed',
+                'top': '30px',
+            });
         } else {
             $('nav').css({
                 'z-index': '0',
                 'position': 'relative',
+                'z-index': 2
 
             });
-            $('.logo').css('z-index', '1');
+            $('.logo').css('z-index', '3');
+            $('aside').css({
+                'position': 'relative',
+                'top': '0px',
+            });
         }
     });
 
